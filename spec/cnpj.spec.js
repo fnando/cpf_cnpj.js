79b867ca0d83bcb1ca2c9d05a31bbc083a0ebc04
Nando Vieira <fnando.vieira@gmail.com>
2016-11-01 09:07:03 -0200

    Change code style.

--- spec/cnpj.spec.js
+++ spec/cnpj.spec.js
@@ -1,8 +1,9 @@
-var cnpj = require("../lib/cnpj");
-var expect = require("chai").expect;
+const cnpj = require("../lib/cnpj");
+const expect = require("chai").expect;
+const {describe, it} = require("mocha");
 
-describe("CNPJ", function() {
-  it("blacklists common numbers", function() {
+describe("CNPJ", () => {
+  it("blacklists common numbers", () => {
     expect(cnpj.isValid("00000000000000")).not.to.be.ok;
     expect(cnpj.isValid("11111111111111")).not.to.be.ok;
     expect(cnpj.isValid("22222222222222")).not.to.be.ok;
@@ -15,48 +16,48 @@ describe("CNPJ", function() {
     expect(cnpj.isValid("99999999999999")).not.to.be.ok;
   });
 
-  it("rejects falsy values", function() {
+  it("rejects falsy values", () => {
     expect(cnpj.isValid("")).not.to.be.ok;
     expect(cnpj.isValid(null)).not.to.be.ok;
     expect(cnpj.isValid(undefined)).not.to.be.ok;
   });
 
-  it("validates formatted strings", function() {
+  it("validates formatted strings", () => {
     expect(cnpj.isValid("54.550.752/0001-55")).to.be.ok;
   });
 
-  it("validates unformatted strings", function() {
+  it("validates unformatted strings", () => {
     expect(cnpj.isValid("54550752000155")).to.be.ok;
   });
 
-  it("validates messed strings", function() {
+  it("validates messed strings", () => {
     expect(cnpj.isValid("54550[752#0001..$55")).to.be.ok;
   });
 
-  it("strictly validates strings", function() {
+  it("strictly validates strings", () => {
     expect(cnpj.isValid("54550[752#0001..$55", true)).not.to.be.ok;
     expect(cnpj.isValid("54.550.752/0001-55", true)).to.be.ok;
     expect(cnpj.isValid("54550752000155", true)).to.be.ok;
   });
 
-  it("returns stripped number", function() {
+  it("returns stripped number", () => {
     var number = cnpj.strip("54550[752#0001..$55");
     expect(number).to.eql("54550752000155");
   });
 
-  it("returns formatted number", function() {
+  it("returns formatted number", () => {
     var number = cnpj.format("54550752000155");
     expect(number).to.eql("54.550.752/0001-55");
   });
 
-  it("generates formatted number", function() {
+  it("generates formatted number", () => {
     var number = cnpj.generate(true);
 
     expect(number).to.match(/^(\d{2}).(\d{3}).(\d{3})\/(\d{4})-(\d{2})$/);
     expect(cnpj.isValid(number)).to.be.ok;
   });
 
-  it("generates unformatted number", function() {
+  it("generates unformatted number", () => {
     var number = cnpj.generate();
 
     expect(number).to.match(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/);
--- spec/cpf.spec.js
+++ spec/cpf.spec.js
@@ -1,8 +1,9 @@
-var cpf = require("../lib/cpf");
-var expect = require("chai").expect;
+const cpf = require("../lib/cpf");
+const expect = require("chai").expect;
+const {describe, it} = require("mocha");
 
-describe("CPF", function() {
-  it("blacklists common numbers", function() {
+describe("CPF", () => {
+  it("blacklists common numbers", () => {
     expect(cpf.isValid("00000000000")).not.to.be.ok;
     expect(cpf.isValid("11111111111")).not.to.be.ok;
     expect(cpf.isValid("22222222222")).not.to.be.ok;
@@ -16,48 +17,48 @@ describe("CPF", function() {
     expect(cpf.isValid("12345678909")).not.to.be.ok;
   });
 
-  it("rejects falsy values", function() {
+  it("rejects falsy values", () => {
     expect(cpf.isValid("")).not.to.be.ok;
     expect(cpf.isValid(null)).not.to.be.ok;
     expect(cpf.isValid(undefined)).not.to.be.ok;
   });
 
-  it("validates formatted strings", function() {
+  it("validates formatted strings", () => {
     expect(cpf.isValid("295.379.955-93")).to.be.ok;
   });
 
-  it("validates unformatted strings", function() {
+  it("validates unformatted strings", () => {
     expect(cpf.isValid("29537995593")).to.be.ok;
   });
 
-  it("validates messed strings", function() {
+  it("validates messed strings", () => {
     expect(cpf.isValid("295$379\n955...93")).to.be.ok;
   });
 
-  it("strictly validates strings", function() {
+  it("strictly validates strings", () => {
     expect(cpf.isValid("295$379\n955...93", true)).not.to.be.ok;
     expect(cpf.isValid("295.379.955-93", true)).to.be.ok;
     expect(cpf.isValid("29537995593", true)).to.be.ok;
   });
 
-  it("returns stripped number", function() {
+  it("returns stripped number", () => {
     var number = cpf.strip("295.379.955-93");
     expect(number).to.eql("29537995593");
   });
 
-  it("returns formatted number", function() {
+  it("returns formatted number", () => {
     var number = cpf.format("29537995593");
     expect(number).to.eql("295.379.955-93");
   });
 
-  it("generates formatted number", function() {
+  it("generates formatted number", () => {
     var number = cpf.generate(true);
 
     expect(number).to.match(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/);
     expect(cpf.isValid(number)).to.be.ok;
   });
 
-  it("generates unformatted number", function() {
+  it("generates unformatted number", () => {
     var number = cpf.generate();
 
     expect(number).to.match(/^\d{3}\d{3}\d{3}\d{2}$/);
